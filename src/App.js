50662bfaf90a3e5fd860de8199e18a7b23e02261
Badhan112 <badhancsesastc112@gmail.com>
2021-02-23 14:21:27 +0600

    dynamic jsx code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const courseName = 'React JS';
+  const maleSinger = {
+    name: 'Dr. Mahafuz',
+    job: 'Singer'
+  }
+  const femaleSinger = {
+    name: 'Eva Rahman',
+    job: 'Kokil Konthi',
+  }
+  const style = {
+    color: 'red',
+    backgroundColor: 'yellow',
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +31,12 @@ function App() {
         >
           Learn React
         </a>
+        <p>My first React Paragraph</p>
+        <h1>Heading no {2+4}</h1>
+        <p>Complex Math result: {(5+8)*3}</p>
+        <p>Course Name: {courseName}</p>
+        <p className="" style={style}>Name: {maleSinger.name}, Job: {maleSinger.job}</p>
+        <p style={{backgroundColor: "cyan", color: "darkblue"}}>Name: {femaleSinger.name}, Job: {femaleSinger.job}</p>
       </header>
     </div>
   );
